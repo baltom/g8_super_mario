5b5098ec0c61a1bede57e0283a734539d0359d51
handan14 <handan14@student.westerdals.no>
2015-04-29 14:41:43 +0200

    Lagde enemies (Goomba og Koopa Troopas)

---
+++ Assets/Enemies/Scripts/GoombaScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoombaScript : MonoBehaviour {
+
+    Vector2 dir;
+    bool dead = false;
+
+	// Use this for initialization
+	void Start () {
+        dir = -Vector2.right;
+    }
+	
+	// Update is called once per frame
+    void Update() {
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, pos + (dir * 0.1f), Time.deltaTime * 0.5f);
+        Vector2 rayPos = new Vector2(transform.position.x + (dir.x * 0.08f), transform.position.y - 0.04f);
+        RaycastHit2D hit = Physics2D.Raycast(rayPos, dir, 0.01f);
+        if (hit.transform != null && !hit.transform.gameObject.tag.Equals("Player") && !dead) {
+            toggleDirection();
+        }
+        RaycastHit2D hit2 = Physics2D.Raycast(new Vector2(rayPos.x - (dir.x * 0.16f), rayPos.y), -dir, 0.01f);
+        if (hit.collider != null && hit.transform.gameObject.tag.Equals("Shell") || hit2.collider != null && hit2.transform.gameObject.tag.Equals("Shell")) {
+            dir = Vector2.zero;
+            transform.localScale = new Vector3(1, -1);
+            transform.position = new Vector3(transform.position.x, transform.position.y);
+            transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 1));
+            Component.Destroy(transform.GetComponent<BoxCollider2D>());
+            dead = true;
+        }
+        Vector2 rayPosUp = new Vector2(pos.x, pos.y + 0.08f);
+        RaycastHit2D hitUp = Physics2D.Raycast(rayPos, Vector2.up, 0.01f);
+        if (hitUp.collider != null && hitUp.transform.gameObject.tag.Equals("Player") || Input.GetKey(KeyCode.Space)) {
+            GameObject deadArt = transform.Find("dead").gameObject;
+            deadArt.SetActive(true);
+            deadArt.transform.SetParent(null);
+            GameObject.Destroy(this.gameObject);
+            GameObject.Destroy(deadArt, 2);
+        }
+	}
+
+    private void toggleDirection() {
+        if (dir == -Vector2.right) {
+            dir = Vector2.right;
+        } else {
+            dir = -Vector2.right;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag.Equals("Player")) {
+            //player.die;
+        }
+    }
+}
---
+++ Assets/Enemies/Scripts/KoopaDeadScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KoopaDeadScript : MonoBehaviour {
+
+    bool shot = false;
+    Vector2 dir;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (shot) {
+            Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+            transform.position = Vector2.MoveTowards(transform.position, pos + (dir * 0.1f), Time.deltaTime * 2);
+            Vector2 rayPos = new Vector2(transform.position.x + (dir.x * 0.08f), transform.position.y - 0.04f);
+            RaycastHit2D hit = Physics2D.Raycast(rayPos, dir, 0.01f);
+            if (hit.transform != null && !hit.transform.gameObject.tag.Equals("Enemy")) {
+                toggleDirection();
+            }
+            RaycastHit2D hit2 = Physics2D.Raycast(rayPos, -dir, 0.01f);
+            if (hit.collider != null && hit.transform.gameObject.tag.Equals("Player") || hit2.collider != null && hit2.transform.gameObject.tag.Equals("Player")) {
+                //Player.die;
+            }
+        }
+	}
+
+    private void toggleDirection() {
+        if (dir == -Vector2.right) {
+            dir = Vector2.right;
+        } else {
+            dir = -Vector2.right;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag.Equals("Player")) {
+            if (collision.transform.position.x > transform.position.x) {
+                dir = -Vector2.right;
+            } else {
+                dir = -Vector2.right;
+            }
+        }
+        shot = true;
+        gameObject.tag = "Shell";
+    }
+}

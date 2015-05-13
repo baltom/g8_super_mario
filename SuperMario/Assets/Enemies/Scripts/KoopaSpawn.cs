4e5b19e3d72af6bea7293fb54ab1420c9f22c7c6
handan14 <handan14@student.westerdals.no>
2015-05-13 12:29:39 +0200

    Fant en måte å legge inn enemies når kameraet treffer dem

---
+++ SuperMario/Assets/Enemies/Scripts/KoopaSpawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KoopaSpawn : MonoBehaviour {
+
+    private Object Koompa;
+
+    // Use this for initialization
+    void Start() {
+        Koompa = UnityEditor.AssetDatabase.LoadMainAssetAtPath("Assets/Enemies/KoopaTroopa.prefab");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Vector2.Distance(transform.position, Camera.main.transform.position) < Camera.main.orthographicSize * 2) {
+            Instantiate(Koompa, transform.position, Quaternion.identity);
+            GameObject.Destroy(this);
+        }
+    }
+}
